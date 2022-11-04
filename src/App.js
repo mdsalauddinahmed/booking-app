599ce43f201942e5eb22659e4938678ac0ccc33c
salauddinAmir <mdsalauddinahmed.amir@gmail.com>
2022-11-05 01:51:33 +0600

    header section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './App.css';
+import Home from "./Pages/Home/Home";
+import Hotel from "./Pages/Hotel/Hotel";
+import List from "./Pages/list/List";
 
 function App() {
   return (
-    <div className="App">
-            <h1>helllo word</h1>
-    </div>
+     <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/hotels" element={<List/>}></Route>
+      <Route path="/hotels/:id" element={<Hotel/>}></Route>
+      </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../Components/Header/Header';
+import Navbar from '../../Components/Navbar/Navbar';
+ import './Home.css';
+const Home = () => {
+    return (
+        <div>
+           <Navbar></Navbar>
+            <Header></Header>
+        </div>
+    );
+};
+
+export default Home;
